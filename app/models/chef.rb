39768c9f96bb429e8e53a9c637d5fee5dbc00658
sk <thanks@no.spam>
2013-04-26 03:08:47 -0400

    chef & user models

---
+++ app/models/chef.rb
@@ -0,0 +1,7 @@
+class Chef < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name, :city
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 AsiachefCh::Application.routes.draw do
+  devise_for :users
+  devise_for :chefs
+
   root :to => 'home#index'
 end
